e337056b1de8769338dfe8bd313ef8e38a7cb8f6
raval786 <33389215+raval786@users.noreply.github.com>
2019-12-21 19:12:06 +0530

    Create p1_random_forest_regression.py

---
+++ regression/random forest regression/p1_random_forest_regression.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 00:15:57 2019
+
+@author: Raval
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+
+
+# Fitting the Random forest regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300 ,random_state= 0)
+regressor.fit(X,y)
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+
+
+# Visualising the Random forest regression results (for higher resolution and smoother curve) not continious model
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.show()
